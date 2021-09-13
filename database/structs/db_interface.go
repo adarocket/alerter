a91558c1b43dbfa33b9240e04f820a28fc9c6489
KatodForAnod <kukushkanezabudka@yandex.by>
2021-09-13 09:43:06 +0300

    update db_interface

--- database/structs/db_interface.go
+++ database/structs/db_interface.go
@@ -2,7 +2,10 @@ package structs
 
 type Database interface {
 	GetDataFromAlerts() ([]AlertsTable, error)
-	GetDataFromAlertNode(alertId int64) ([]AlertNodeTable, error)
+	GetDataFromAlertNode(alertId int64) (AlertNodeTable, error)
+	GetDataFromAlert(id int64) (AlertsTable, error)
+	UpdateDataInAlertsTable(table AlertsTable) error
+	UpdateDataInAlertNode(table AlertNodeTable) error
 	FillTables() error
 	CreateTables() error
 }
