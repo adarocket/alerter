c3fa677799a3b3aa7d7492107a57d7201088ca74
nikitamakarchuk <kukushkanezabudka@yandex.by>
2021-12-14 10:15:53 +0300

    rebuild cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,65 +1,60 @@
 package cache
 
 import (
+	"errors"
+	"log"
 	"sync"
 
 	"github.com/adarocket/proto/proto-gen/cardano"
 	"github.com/adarocket/proto/proto-gen/chia"
 )
 
-// FIXME: переделать кэширование
-// использовать https://github.com/dgraph-io/ristretto
-
 var (
 	once     sync.Once
 	instance Cache
 )
 
-// GetCacheInstance - возвращает или создает объект кэша, проинициализировав перед этим карты
+type Cache struct {
+	CardanoNodes map[string]*cardano.SaveStatisticRequest
+	ChiaNodes    map[string]*chia.SaveStatisticRequest
+}
+
 func GetCacheInstance() Cache {
 	once.Do(func() {
 		instance = Cache{}
-		instance.CardanoNodes = make(map[KeyCache]*cardano.SaveStatisticRequest)
+		instance.CardanoNodes = make(map[string]*cardano.SaveStatisticRequest)
+
 	})
 
 	return instance
 }
 
-type KeyCache struct {
-	Key      string
-	TypeNode string
-}
-
-type Cache struct {
-	CardanoNodes map[KeyCache]*cardano.SaveStatisticRequest
-	ChiaNodes    map[KeyCache]*chia.SaveStatisticRequest
-}
-
-// AddNewInform - аргументом является карта, функции проверяет наличие такого типа и
-// если он есть делает пересечение карт
-func (c *Cache) AddNewInform(newMap interface{}) {
-	if newMapCardano, isTr := newMap.(map[KeyCache]*cardano.SaveStatisticRequest); isTr {
+func (c *Cache) AddCardanoData(data interface{}) error {
+	if newMapCardano, true := data.(map[string]*cardano.SaveStatisticRequest); true {
 		for key, node := range newMapCardano {
 			c.CardanoNodes[key] = node
 		}
+		return nil
 	}
 
-	if newMapChia, isTr := newMap.(map[KeyCache]*chia.SaveStatisticRequest); isTr {
-		for key, node := range newMapChia {
-			c.ChiaNodes[key] = node
-		}
+	if newCardanoNode, true := data.(*cardano.SaveStatisticRequest); true {
+		uuid := newCardanoNode.NodeAuthData.Uuid
+		c.CardanoNodes[uuid] = newCardanoNode
+		return nil
 	}
-	// add error if type not exist
+
+	return errors.New("invalid type, only mapNodes and node supported")
 }
 
-func (c *Cache) GetOldNodeByType(newNode interface{}, key KeyCache) (oldNode interface{}) {
-	if _, isTr := newNode.(*cardano.SaveStatisticRequest); isTr {
-		return c.CardanoNodes[key]
+func (c *Cache) GetCardanoNodes(uuids []string) map[string]*cardano.SaveStatisticRequest {
+	result := make(map[string]*cardano.SaveStatisticRequest)
+	for _, uuid := range uuids {
+		if node, isExist := c.CardanoNodes[uuid]; isExist {
+			result[uuid] = node
+		} else {
+			log.Println("node uuid: ", uuid, "does not exist")
+		}
 	}
 
-	if _, isTr := newNode.(*chia.SaveStatisticRequest); isTr {
-		return c.ChiaNodes[key]
-	}
-
-	return nil
+	return result
 }
