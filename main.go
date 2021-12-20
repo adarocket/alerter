dfa7ed1f57b12d1443d413053887a5e35736a370
nikitamakarchuk <kukushkanezabudka@yandex.by>
2021-12-20 08:04:15 +0300

    removed conf, update main

--- main.go
+++ main.go
@@ -2,37 +2,30 @@ package main
 
 import (
 	"embed"
-	"fmt"
-	"github.com/Knetic/govaluate"
-	"reflect"
+	"github.com/adarocket/alerter/internal"
+	"github.com/adarocket/alerter/internal/config"
+	"github.com/adarocket/alerter/internal/database/controller"
+	"github.com/adarocket/alerter/internal/database/model"
+	"log"
 )
 
 //go:embed tmpl/*.html
 var webUI embed.FS
 
 func main() {
-	expression, _ := govaluate.NewEvaluableExpression("10 ** 0.5")
-
-	parameters := make(map[string]interface{}, 8)
-	parameters["foo"] = -1
-
-	result, _ := expression.Evaluate(parameters)
-	fmt.Println(reflect.TypeOf(result), result)
-
-	/*log.SetFlags(log.Lshortfile)
+	log.SetFlags(log.Lshortfile)
 	conf, err := config.LoadConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	dbConn := database.InitDatabase(conf.SqlLitePathDB)
+	dbConn := model.InitDatabase(conf.SqlLitePathDB)
+
 	controller.InitializeControllerInstances(dbConn)
 	//database.CreateTables()
 	//database.FillTables()
 
-	web.WebUI = webUI
-	go func() {
-		nodesinfo.StartTracking(conf.TimeoutCheck, conf.NotifierAddr)
-	}()
-	web.StartServer(conf.WebServerAddr)*/
+	internal.StartTracking(conf, dbConn)
+	//web.WebUI = webUI
+	//web.StartServer(conf.WebServerAddr)
 }
